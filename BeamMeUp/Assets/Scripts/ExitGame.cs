8d359d3794623d2ca96026a60948f1d811d6e983
TheAlot <viktorvase@gmail.com>
2019-04-28 12:43:11 +0200

    THE FULL GAME IS COMPLETE

--- BeamMeUp/Assets/Scripts/ExitGame.cs
+++ BeamMeUp/Assets/Scripts/ExitGame.cs
@@ -6,6 +6,10 @@ public class ExitGame : MonoBehaviour
 {
     public void Exit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
--- BeamMeUp/Assets/Scripts/LevelManager.cs
+++ BeamMeUp/Assets/Scripts/LevelManager.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class LevelManager : MonoBehaviour
 {
     public static int points = 0, pointsToWin = 3;
     public SceneFader sceneFader;
     private GameObject startBlock;
+    private List<GameObject> pointsUI;
     public CameraController freeCam;
     // Start is called before the first frame update
     void Start()
     {
         startBlock = GameObject.FindGameObjectWithTag("StartBlock");
+        pointsUI = new List<GameObject>();
+        pointsUI.Add(GameObject.Find("Point1"));
+        pointsUI.Add(GameObject.Find("Point2"));
+        pointsUI.Add(GameObject.Find("Point2"));
+        points = 0;
     }
 
     private void Update()
@@ -22,11 +29,16 @@ public class LevelManager : MonoBehaviour
         }
     }
     public void ReachedEnd(){
+        pointsUI[points].GetComponent<Image>().color = Color.cyan;
         points++;
         if(points >= pointsToWin){
             Debug.Log("You have won");
-            //int scene = SceneManager.GetSceneByName("MainMenu").buildIndex;
-            sceneFader.ChangeScene("LevelSelect");
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            if(sceneIndex >= SceneManager.sceneCountInBuildSettings)
+            {
+                sceneFader.ChangeScene("MainMenu");
+            }
+            sceneFader.ChangeScene("Level" + sceneIndex);
             points = 0;
             return;
         }
